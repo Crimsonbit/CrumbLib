9cd18f629870bbb30541d3f40d54ff68718020cd
Alex Daum <daumalex10200@gmail.com>
2017-11-21 17:44:40 +0100

    Added first functions

--- crumblib.c
+++ crumblib.c
@@ -1,4 +1,23 @@
+#include "crumblib.h"
 #include <avr/io.h>
+const uint8_t TIMER0 = 0;
 
+void setNearestPrescaler(volatile uint8_t *, uint8_t , uint16_t, const uint16_t *, uint8_t);
+
+CRUMBLIB_TIMER(0, led_blink1);
 int main() {
+	crumblib_timer_led_blink1_disable();	
+}
+
+void setNearestPrescaler(volatile uint8_t *TCCRB, uint8_t CSx0, uint16_t presc, const uint16_t *selectable_prescs, uint8_t numPresc){
+	uint8_t i;
+	for(i = numPresc; i > 0; i--){
+		if(selectable_prescs[i] > presc){
+			i = i & 0b111;
+			//Assume CS Bits are consecutive in TCCRxB
+			i <<= CSx0; 
+			*TCCRB |= i;
+			return;
+		}
+	}
 }
--- crumblib.h
+++ crumblib.h
@@ -1,8 +1,26 @@
-#ifndef _CRUMBLIB_INCLUDED_
+#ifndef _CRUMBLIB_INCLUDED
 
+#ifndef __AVR_ATmega644PA__
+#define __AVR_ATmega644PA__
+#endif
+
+#include <avr/io.h>
 #define _CRUMBLIB_INCLUDED_
 
-typedef enum {LOW_LEVEL, FALLING_EDGE, RISING_EDGE, ANY_EDGE} CrumbLib_Interrupt_Type;
+#define TIMER0_PRESCS 1, 8, 64, 256, 1024
+#define TIMER1_PRESCS 1, 8, 64, 256, 1024
+#define TIMER2_PRESCS 1, 8, 32, 64, 128, 256, 1024
 
+#define CRUMBLIB_TIMER(Number, name)                                       \
+	static inline void crumblib_timer_##name##_disable(void) {             \
+		TCCR##Number##B &= ~(1 << CS##Number##2) & ~(1 << CS##Number##1) & \
+		                   ~(1 << CS##Number##0);                            \
+		(void)crumblib_timer_##name##_disable;                             \
+	} \
+	static inline void crumblib_timer_##Number##_set_presc(uint16_t presc) \
+{  \
+	const uint16_t a[] = { TIMER##Number##_PRESCS };\
+	setNearestPrescaler(&TCCR##Number##B, CS##Number##0, presc, a, sizeof(a)/sizeof(a[0]));\
+}  \
 
 #endif
